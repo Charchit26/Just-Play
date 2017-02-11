814d1564ecbb2dd8dacb776cc211a09efd54cad9
Charchit26 <charchit26@gmail.com>
2017-02-11 15:35:27 +0530

    IntelliJ added select dir option

--- main.js
+++ main.js
@@ -4,15 +4,20 @@ const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 var mainWindow = null;
+var firstWindow=null;
+const ipc = require('electron').ipcMain;
 app.on('window-all-closed', function() {
     if (process.platform != 'darwin') {
         app.quit();
     }
 });
 app.on('ready', function() {
-	  mainWindow = new BrowserWindow({width: 450, height: 400, resizable: false});
-	  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
-	  mainWindow.on('closed', function() {
-	    mainWindow = null;
-	  });
+	mainWindow = new BrowserWindow({width: 450, height: 450, resizable: false});
+	mainWindow.loadURL('file://' + __dirname + '/app/getDir.html');
+	mainWindow.on('closed', function() {
+		mainWindow = null;
 	});
+});
+ipc.on('load-page', (event, arg) => {
+    mainWindow.loadURL(arg);
+});
